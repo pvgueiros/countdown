4db238b2fe0e001f1eaefc586ec455817cf04200
Paula Vasconcelos Gueiros <pvgueiros@gmail.com>
2025-11-07 19:37:11 -0600

    Initial Xcode Project

---
+++ Countdown/CountdownApp.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownApp.swift
+//  Countdown
+//
+//  Created by Paula Vasconcelos Gueiros on 3/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
