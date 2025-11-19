aad8b606af76774c7f2224313559fea5d685d4d2
Paula Vasconcelos Gueiros <pvgueiros@gmail.com>
2025-11-19 11:59:57 -0500

    CI: enable coverage in test plan and pass -testPlan; collect app target coverage

--- scripts/ci/build-and-test.sh
+++ scripts/ci/build-and-test.sh
@@ -6,11 +6,13 @@ PROJECT="Countdown.xcodeproj"
 DESTINATION="platform=iOS Simulator,name=iPhone 15"
 DERIVED_DATA="$(pwd)/DerivedData"
 RESULT_BUNDLE="$(pwd)/TestResults.xcresult"
+TEST_PLAN="TestPlan"
 
 rm -rf "$DERIVED_DATA" "$RESULT_BUNDLE"
 xcodebuild \
   -project "$PROJECT" \
   -scheme "$SCHEME" \
+  -testPlan "$TEST_PLAN" \
   -destination "$DESTINATION" \
   -derivedDataPath "$DERIVED_DATA" \
   -resultBundlePath "$RESULT_BUNDLE" \
